cc605cc6ebd26108b97e4a38b7096b6bb714f332
kimjw0427 <63496362+kimjw0427@users.noreply.github.com>
2023-04-03 13:35:12 +0900

    Update add.py

--- challenge/add.py
+++ challenge/add.py
@@ -1,3 +1,27 @@
-for i in range(0,10):
-    a = int(input())
-    a
+from executer import *
+import random
+
+def start_challenge(target_file):
+    comp = 0
+    for i in range(0,10):
+        p = process(target_file)
+
+        v1 = random.randint(0,10**10); v2 = random.randint(0,10**10)
+        sendline(p,f"{v1} {v2}".encode())
+        ret = readline(p)
+
+        if int(ret) == (v1+v2):
+            comp = comp + 1
+        p.kill()
+    
+    if comp==10:
+        return 1
+    else:
+        return 0
+    
+
+if start_challenge(["python3","test.py"]):
+    print("성공")
+else:
+    print("실패")
+
